f5ee96ced63f86d0e537a26ceee9f293e93b68d1
Konstantin Enchant <sirkonst@gmail.com>
2016-09-06 16:15:31 +0300

    simple setup.py

--- setup.py
+++ setup.py
@@ -1,87 +1,16 @@
 """`Dependency injector` setup script."""
-
-import os
-import re
-
 from setuptools import setup
-from setuptools import Command
-
-
-# Getting description:
-with open('README.rst') as readme_file:
-    description = readme_file.read()
-
-# Getting requirements:
-with open('requirements.txt') as version:
-    requirements = version.readlines()
-
-# Getting version:
-with open('dependency_injector/__init__.py') as init_file:
-    version = re.search('VERSION = \'(.*?)\'', init_file.read()).group(1)
-
-
-class PublishCommand(Command):
-    """Setuptools `publish` command."""
-
-    description = "Publish current distribution to PyPi and create tag"
-    user_options = []
-
-    def initialize_options(self):
-        """Init options."""
-
-    def finalize_options(self):
-        """Finalize options."""
-
-    def run(self):
-        """Command execution."""
-        self.run_command('sdist')
-        self.run_command('upload')
-        os.system('git tag -a {0} -m \'version {0}\''.format(version))
-        os.system('git push --tags')
 
 
 setup(name='acr-dependency-injector',
-      version=version,
+      version='0.1',
       description='Python dependency injection framework',
-      long_description=description,
+      long_description='Temporary package',
       author='ETS Labs',
       author_email='rmogilatov@gmail.com',
-      maintainer='Konstantin Enchant',
-      maintainer_email='sirkonst@gmail.com',
-      url='https://github.com/sirkonst/python-dependency-injector/tree/acr',
-      bugtrack_url='https://github.com/ets-labs/python-dependency-injector' +
-                   '/issues',
       license='BSD New',
       packages=['dependency_injector',
                 'dependency_injector.providers'],
-      platforms=['any'],
-      zip_safe=True,
-      install_requires=requirements,
-      cmdclass={
-          'publish': PublishCommand,
-      },
-      keywords=[
-          'DI',
-          'Dependency injection',
-          'IoC',
-          'Inversion of Control',
-      ],
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: BSD License',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: Implementation :: CPython',
-          'Programming Language :: Python :: Implementation :: PyPy',
-          'Topic :: Software Development',
-          'Topic :: Software Development :: Libraries',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-      ])
+      install_requires=['six'],
+      url='https://github.com/sirkonst/python-dependency-injector/tree/acr',
+)
